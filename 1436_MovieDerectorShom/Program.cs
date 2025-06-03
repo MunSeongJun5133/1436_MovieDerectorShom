567923034e56baef76e402c12554180b390d29a2
MunSeongJun5133 <ans1332@naver.com>
2025-06-03 20:24:41 +0900

    초기 커밋.

---
+++ 1436_MovieDerectorShom/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1436_MovieDerectorShom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 n = Convert.ToInt32(Console.ReadLine());
+            Int32 count = 0;
+            Int32 start = 666;
+
+            while (true)
+            {
+                if (count >= n)
+                    break;
+
+                String sLen = Convert.ToString(start);
+                for (Int32 i = 0; i < sLen.Length - 2; i++)
+                {
+                    if (sLen[i] == '6')
+                    {
+                        if (sLen[i] == '6' && sLen[i + 1] == '6' && sLen[i + 2] == '6')
+                        {
+                            count++;
+                            break;
+                        }
+                    }
+                }
+                if(count < n)
+                    start++;
+            }
+
+            Console.WriteLine(start);
+        }
+    }
+}
